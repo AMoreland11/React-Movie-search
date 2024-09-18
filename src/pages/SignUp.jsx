1ac75e74e103cf171526e56ca401cdfe63984c34
AMoreland11 <a11daaron@gmail.com>
2024-09-18 16:37:21 -0400

    signup and login pages

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import '../pages/SignUpStyle.css';
+
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>Sign Up</h1>
-      {/* Add form and input fields for sign up */}
-      <form>
-        <label>Username: </label>
-        <input type="text" name="username" /><br />
-        <label>Password: </label>
-        <input type="password" name="password" /><br />
-        <label>Email: </label>
-        <input type="email" name="email" /><br />
-        <button type="submit">Sign Up</button>
-      </form>
+    <div className='signUp__wrapper'>
+        <div className='signUp__container'>
+            <h1>Sign Up</h1>
+            <form>
+                <div className='signUp__user'>
+                    <label>Username: </label>
+                    <input type="text" name="username" />
+                </div>
+                <div className='signUp__pass'>
+                    <label>Password: </label>
+                    <input type="password" name="password" />
+                </div>
+                <div className='signUp__email'>
+                    <label>Email: </label>
+                    <input type="email" name="email" />
+                </div>
+                <button className='login'type="submit">Sign Up</button>
+            </form>
+        </div>
     </div>
   );
 };
